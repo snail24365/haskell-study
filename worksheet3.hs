2bf231826ac165b9a40ec1dc18012d5bb58ef353
snail24365 <snail24365@gmail.com>
2021-02-22 16:01:58 +0900

    added exercise worksheet

---
+++ worksheet3.hs
@@ -0,0 +1,27 @@
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+productC :: [Int] -> Int
+productC [] = 1
+productC (x:xs) = x * productC xs
+
+lengthC :: [a] -> Int
+lengthC [] = 0
+lengthC (x:xs) = 1+ lengthC xs
+
+reverseC :: [a] -> [a]
+reverseC [] = []
+reverseC (x:xs) = reverse(xs) ++ x
+
+
+나는 어떻게 살고 싶은가.
+
+관세공무원으로서 한 공직을 맡고있는 것에 대해 자긍심을 느끼며 삶에 보람을 느끼며 근무하고 싶습니다.
+
+저는 일을 여러가지 벌린적은 많았으나 마무리하지 못할 때가 많았고, 그때마다 아쉬움을 느겼습니다.
+그래서 시작한 일을 끈기있게 실행하여 결실을 맺는 삶을 살고 싶습니다.
+
+
+지피지기 백전불패
+
+
